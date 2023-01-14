a3d6e80f29550f7a2e1a49608d795b68a5385328
Igor Omelchenko <110295552+Derangedwerwolf@users.noreply.github.com>
2023-01-14 09:18:46 +0200

    mult with logger

--- ThredTask/multiprocessingTask/multiprocessingHome.py
+++ ThredTask/multiprocessingTask/multiprocessingHome.py
@@ -1,6 +1,12 @@
 from multiprocessing import cpu_count, Pool, current_process
 #from time import time
+import logging
 import timeit
+
+logger = logging.getLogger()
+stream_handler = logging.StreamHandler()
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
 
 def factorize(*numbers) -> list[list[int]]:
     result = []
@@ -9,7 +15,7 @@ def factorize(*numbers) -> list[list[int]]:
         new_numbers = []
         operand = 1
         
-        print('Starting: ', current_process().name)  
+        logger.debug(f'St:  {current_process().name}')  
         
         while operand <= num:
             if num % operand != 0:
@@ -28,18 +34,9 @@ if __name__ == '__main__':
     
     with Pool(8) as pool:
         pool.apply(factorize, (128, 255, 99999, 10651060))
-        pool.close()
-        pool.join()
-    
-    print('Starting: ', current_process().name)    
-    a, b, c, d  = factorize(128, 255, 99999, 10651060)
 
-    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
-    
-    
+    logger.debug(f'Starting: {current_process().name}')    
+        
     print(f'Working time: {timeit.default_timer() - starttime}')
     print(f'CPU - {cpu_count()}')
 
